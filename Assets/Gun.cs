8babe9e1149310d1cbdd0fc95e3bf773aea4d2ba
Game1044 <patipat.tong@bumail.net>
2025-05-08 02:32:19 +0700

    good

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -6,7 +5,7 @@ public class Gun : MonoBehaviour
     public Camera cam;
     public float range = 100f;
     public AudioSource gunSound;
-    public RectTransform hitMarkerUI; // เป้าแบบ UI
+    public RectTransform hitMarkerUI;
 
     void Update()
     {
@@ -14,30 +13,22 @@ public class Gun : MonoBehaviour
         {
             gunSound.Play();
 
-            Vector3 screenPos = Input.mousePosition;
-            Ray ray = cam.ScreenPointToRay(screenPos);
+            // แสดงเป้าในตำแหน่งที่ผู้เล่นคลิก
 
+
+            // ตรวจสอบว่ากระสุนโดนอะไรหรือไม่
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    
             if (Physics.Raycast(ray, out RaycastHit hit, range))
             {
                 Bottle bottle = hit.collider.GetComponent<Bottle>();
                 if (bottle != null)
                 {
                     bottle.OnHit();
-
-                    // แปลงตำแหน่ง world ที่โดน ไปเป็นตำแหน่งบนหน้าจอ
-                    Vector3 uiPosition = cam.WorldToScreenPoint(hit.point);
-                    hitMarkerUI.position = uiPosition;
-
-                    StartCoroutine(ShowHitMarker());
                 }
             }
         }
     }
 
-    IEnumerator ShowHitMarker()
-    {
-        hitMarkerUI.gameObject.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
-        hitMarkerUI.gameObject.SetActive(false);
-    }
+
 }
